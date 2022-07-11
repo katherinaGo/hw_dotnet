e47a2d8a83eb7e25c074a17ed2f144e07625d80e
katherinaGo <govinkatherina@gmail.com>
2022-07-11 19:11:16 +0300

    Added task 2

---
+++ CSharp.Homeworks/HW.07.Task2.AssemblyTwo/Program.cs
@@ -0,0 +1,10 @@
+﻿using HW._07.Task2.AssemblyOne;
+
+// Не могу создать объект класса, так как класс находится в другой сборке
+// Motorcycle yamaha = new Motorcycle();
+// SportBike bike1 = new SportBike();
+
+PublicBike bike = new PublicBike();
+
+bike.StartEngine();
+bike.FixBike();
